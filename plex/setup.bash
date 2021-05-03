c860d0e5fc01cd79b18733d54224d267645d50ca
Tim Delesio <tdelesio@gmail.com>
2021-05-03 19:49:42 -0400

    added bash file

---
+++ plex/setup.bash
@@ -0,0 +1,9 @@
+mkdir /mnt/storage/appdata/
+mkdir /mnt/storage/media/tv
+mkdir /mnt/storage/media/movies
+mkdir /mnt/storage/media/christmas
+mkdir /mnt/storage/media/kids
+mkdir /mnt/storage/media/4kmovies
+mkdir /mnt/storage/tmp
+mkdir /mnt/storage/appdata/downloads/complete
+mkdir /mnt/storage/media/books/ebooks
